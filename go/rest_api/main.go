d5f39d6c8139bad70d0ac564bc31df0aeb2f322b
vnpedroso <npp.vinicius@gmail.com>
2025-11-11 19:32:47 -0300

    creating main.go to run webserver

---
+++ go/rest_api/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+var cdb *characterDb
+
+func init() {
+	cdb = &characterDb{
+		db: make(map[string]characterJSON),
+	}
+}
+
+func CharacterMultiplex(rw http.ResponseWriter, req *http.Request) {
+	//multiplex handlerFunc for the /characters endpoint
+	switch req.Method {
+	case http.MethodGet:
+		getAllChars(rw, req)
+	case http.MethodPost:
+		createChar(rw, req)
+	default:
+		http.Error(rw, "method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+}
+
+func CharacterIdMultiplex(rw http.ResponseWriter, req *http.Request) {
+	//multiplexer handlerFunc for the /characters/{id} endpoint
+	switch req.Method {
+	case http.MethodGet:
+		getCharById(rw, req)
+	case http.MethodDelete:
+		deleteChar(rw, req)
+	case http.MethodPut:
+		updateChar(rw, req)
+	default:
+		http.Error(rw, "method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+}
+
+func main() {
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+
+	http.HandleFunc("/", index)
+	http.HandleFunc("/docs", docs)
+	http.HandleFunc("/characters", CharacterMultiplex)
+	http.HandleFunc("/characters/{id}", CharacterIdMultiplex)
+
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
