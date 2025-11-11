5312d2361d58f837e67c84f840c27f2fa7105a65
vnpedroso <npp.vinicius@gmail.com>
2025-11-11 19:19:48 -0300

    representation of the API resources & collections

---
+++ go/rest_api/types.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"errors"
+	"sync"
+)
+
+type characterJSON struct {
+	Id         string `json:"id,omitempty"`
+	Name       string `json:"name"`
+	Class      string `json:"class"`
+	Race       string `json:"race"`
+	Level      int    `json:"level"`
+	MainWeapon string `json:"main_weapon,omitempty"`
+}
+
+type characterPayload []characterJSON
+
+type characterDb struct {
+	mtx sync.Mutex
+	db  map[string]characterJSON
+}
+
+func (cdb *characterDb) getAll() characterPayload {
+	cdb.mtx.Lock()
+	defer cdb.mtx.Unlock()
+	out := make(characterPayload, 0, len(cdb.db))
+	for _, v := range cdb.db {
+		out = append(out, v)
+	}
+	return out
+}
+
+func (cdb *characterDb) charExists(hexId string) bool {
+	cdb.mtx.Lock()
+	defer cdb.mtx.Unlock()
+	_, ok := cdb.db[hexId]
+	return ok
+}
+
+func (cdb *characterDb) create(hexId string, char characterJSON) error {
+	ok := cdb.charExists(hexId)
+
+	if !ok {
+		cdb.mtx.Lock()
+		cdb.db[hexId] = char
+		cdb.mtx.Unlock()
+		return nil
+	}
+
+	return errors.New("CharacterAlreadyExists: A character with the provided id already exists")
+}
+
+func (cdb *characterDb) delete(hexId string) error {
+	if cdb.charExists(hexId) {
+		cdb.mtx.Lock()
+		delete(cdb.db, hexId)
+		cdb.mtx.Unlock()
+		return nil
+	}
+
+	return errors.New("CharacterDoesNotExist: A character with the provided id does not exist")
+}
+
+func (cdb *characterDb) get(hexId string) (characterJSON, error) {
+	var char characterJSON
+	cdb.mtx.Lock()
+	char, ok := cdb.db[hexId]
+	cdb.mtx.Unlock()
+	if ok {
+		return char, nil
+	}
+	return char, errors.New("CharacterDoesNotExist: A character with the provided id does not exist")
+}
